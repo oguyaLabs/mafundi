a79da3039c6512fb752279e3cb27c4414964ae97
oguya <oguyajames@gmail.com>
2014-06-06 18:00:26 +0300

    adding lists, dummy data

--- src/com/wglxy/example/dashL/F3Activity.java
+++ src/com/wglxy/example/dashL/F3Activity.java
@@ -16,34 +16,87 @@
 
 package com.wglxy.example.dashL;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.wglxy.example.dashL.adapters.CompanyListAdapter;
+import com.wglxy.example.dashL.adapters.CompanyListAdapter.Inflater;
+
 
 /**
- * This is the activity for feature 3 in the dashboard application.
- * It displays some text and provides a way to get back to the home activity.
- *
- */
-
-public class F3Activity extends DashboardActivity 
-{
-
-/**
- * onCreate
- *
- * Called when the activity is first created. 
- * This is where you should do all of your normal static set up: create views, bind data to lists, etc. 
- * This method also provides you with a Bundle containing the activity's previously frozen state, if there was one.
+ * This is the activity for feature 3 in the dashboard application. It displays
+ * some text and provides a way to get back to the home activity.
  * 
- * Always followed by onStart().
- *
- * @param savedInstanceState Bundle
  */
 
-protected void onCreate(Bundle savedInstanceState) 
-{
-    super.onCreate(savedInstanceState);
-    setContentView (R.layout.activity_f3);
-    setTitleFromActivityLabel (R.id.title_text);
+public class F3Activity extends DashboardActivity implements OnItemClickListener {
+
+	/**
+	 * onCreate
+	 * 
+	 * Called when the activity is first created. This is where you should do
+	 * all of your normal static set up: create views, bind data to lists, etc.
+	 * This method also provides you with a Bundle containing the activity's
+	 * previously frozen state, if there was one.
+	 * 
+	 * Always followed by onStart().
+	 * 
+	 * @param savedInstanceState
+	 *            Bundle
+	 */
+	
+	private CompanyListAdapter adapter;
+	private ListView listView;
+	
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_f3);
+		setTitleFromActivityLabel(R.id.title_text);
+		
+		listView = (ListView)findViewById(R.id.featureResultsList);
+		listView.setOnItemClickListener(this);
+		
+		//dummy data 
+		ArrayList<String> data = new ArrayList<String>();
+		for(int i=0; i<10; i++) data.add("");
+		
+		CompanyListAdapter.Inflater inflater = new Inflater(3);
+		
+		adapter = new CompanyListAdapter(inflater, data);
+		listView.setAdapter(adapter);
+	}
+	
+   @Override
+   public void onPause(){
+       super.onPause();
+   }
+
+   @Override
+   public void onResume(){
+       super.onResume();
+   }
+   
+   @Override
+   protected void onSaveInstanceState(Bundle outState){
+       super.onSaveInstanceState(outState);
+   }
+
+   @Override
+   protected void onRestoreInstanceState(Bundle savedInstanceState){
+   	super.onRestoreInstanceState(savedInstanceState);
+   	
+   }
+
+@Override
+public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	// TODO Auto-generated method stub
+	
 }
-    
+
 } // end class
