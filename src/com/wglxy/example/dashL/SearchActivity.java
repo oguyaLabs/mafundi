0556306c5d56597498474b47904ed35da3b28d0c
oguya <oguyajames@gmail.com>
2014-06-18 19:17:02 +0300

    Search::merging search api endpoint with app, creating net ops

--- src/com/wglxy/example/dashL/SearchActivity.java
+++ src/com/wglxy/example/dashL/SearchActivity.java
@@ -16,21 +16,51 @@
 
 package com.wglxy.example.dashL;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.wglxy.example.dashL.adapters.ViewInflaterBaseAdapter;
 import com.wglxy.example.dashL.constants.Constants;
+import com.wglxy.example.dashL.model.User;
+import com.wglxy.example.dashL.net.NetHandler;
 
 /**
  * This is the Search activity in the dashboard application. It displays some
@@ -59,6 +89,15 @@ public class SearchActivity extends DashboardActivity implements OnItemClickList
 	SearchListAdapter adapter;
     private ListView listView;
     private String searchStr;
+    private ProgressBar loading;
+    private TextView empty_results;
+    
+    private View searchBar;
+    private EditText edt_search;
+    private Button btn_search;
+    
+    private ArrayList<User> data;
+    private boolean loadingError = false;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -66,23 +105,130 @@ public class SearchActivity extends DashboardActivity implements OnItemClickList
 		setTitleFromActivityLabel(R.id.title_text);
 		showLogginLogout(findViewById(R.id.btn_login_logout));
 
+		initUI();
+		data = new ArrayList<User>();
+		
 		try{
 			Bundle args = getIntent().getExtras();
 			searchStr = args.getString(Constants.KEY_SEARCH_ARGS);
+			Log.e(LOG_TAG, "search query: "+searchStr);
+			doSearch(searchStr);
 		}catch(Exception ex){
-			Log.e(LOG_TAG, "no search args: "+ex.getMessage());
+			Log.e(LOG_TAG, "no search args",ex.getCause());
+			searchBar.setVisibility(View.VISIBLE);
+			edt_search.requestFocus();
 		}
-		
-		
+		 
+	}
+	
+	void initUI(){
 		listView = (ListView)findViewById(R.id.searchResultsList);
 		listView.setOnItemClickListener(this);
+		loading = (ProgressBar)findViewById(R.id.loading);
+		empty_results = (TextView)findViewById(R.id.empty_results);
+		searchBar = (LinearLayout)findViewById(R.id.search_bar);
+	    edt_search = (EditText)findViewById(R.id.edt_search);
+	    btn_search = (Button)findViewById(R.id.btn_search);
+	    
+	    btn_search.setOnClickListener(clickListener);
+	    edt_search.addTextChangedListener(watcher);
+	}
+	
+	TextWatcher watcher = new TextWatcher() {
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before, int count) {}
 		
-		//dummy data 
-		ArrayList<String> data = new ArrayList<String>();
-		for(int i=0; i<10; i++) data.add("");
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 		
+		@Override
+		public void afterTextChanged(Editable editable) {
+			if (edt_search.getText().length() > 3){
+				btn_search.setEnabled(true);
+			}else{
+				btn_search.setEnabled(false);
+			}
+		}
+	};
+	
+	View.OnClickListener clickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			switch(view.getId()){
+			case R.id.btn_search:
+				String query = edt_search.getText().toString(); 
+				if(query.length() > 3){
+					doSearch(query);
+				}
+				break;
+			default:break;
+			}
+		}
+	};
+	
+	void setListData(){
 		adapter = new SearchListAdapter(new Inflater(), data);
 		listView.setAdapter(adapter);
+	}
+	
+	void doSearch(String queryStr){
+		String[] args= {queryStr};
+		try {
+			String results = new NetOps().execute(args).get();
+			data = parseJSON(results);
+			if(data.size() != 0 && !loadingError){
+				setListData();
+			}else{
+				toast("Sorry, no results found matching your search criteria");
+				empty_results.setVisibility(View.VISIBLE);
+			}
+		} catch (InterruptedException e) {
+			loadingError = true;
+			toast("something's not right! Please try again later.");
+		} catch (ExecutionException e) {
+			loadingError = true;
+			toast("something's not right! Please try again later.");
+		} catch (JSONException e) {
+			loadingError = true;
+			toast("something's not right! Please try again later.");
+		}
+	}
+	
+	ArrayList<User> parseJSON(String results) throws JSONException{
+		ArrayList<User> list = new ArrayList<User>();
+		JSONObject jsonResult = new JSONObject(results);
+		int  status = jsonResult.getInt("status");
+		int numResults = jsonResult.getInt("numResults");
+		
+		if(status != 200 && numResults <= 0)
+			return list;
+		else{
+			JSONArray arr = jsonResult.getJSONArray("results");
+			Log.e(LOG_TAG, "results: "+arr.length());
+			
+			for(int i=0; i<arr.length(); i++){
+				JSONObject jsonObject = arr.getJSONObject(i);
+				User user = new User();
+				int id = jsonObject.getInt("id");
+				String email = jsonObject.getString("email");
+				String first_name  = jsonObject.getString("first_name");
+				String last_name = jsonObject.getString("last_name");
+				String business_name  = jsonObject.getString("business_name");
+				String services = jsonObject.getString("services");
+				String address = jsonObject.getString("address");
+				
+				user.setId(id);
+				user.setEmail(email);
+				user.setFirst_name(first_name);
+				user.setLast_name(last_name);
+				user.setBusiness_name(business_name);
+				user.setAddress(address);
+				user.setServices(services);
+				
+				list.add(user);
+			}
+			return list;
+		}
 	}
 	
     @Override
@@ -111,7 +257,10 @@ public class SearchActivity extends DashboardActivity implements OnItemClickList
 	public class Inflater implements ViewInflaterBaseAdapter.ViewInflater {
 
 		private class ViewHolder {
-
+			ImageView img_biz_pic;
+			TextView txt_biz_name;
+			TextView txt_biz_loc;
+			RatingBar rb_ratings;
 		}
 
 		@Override
@@ -121,47 +270,30 @@ public class SearchActivity extends DashboardActivity implements OnItemClickList
 			View rowView = ConvertView;
 
 			if (rowView == null) {
-				rowView = inflater.inflate(R.layout.list_item_search, parent,
-						false);
+				rowView = inflater.inflate(R.layout.list_item_search, parent, false);
 
 				ViewHolder viewHolder = new ViewHolder();
-				// viewHolder.section_media =
-				// (LinearLayout)rowView.findViewById(R.id.section_media);
-				// viewHolder.img_profpic = (ImageView)
-				// rowView.findViewById(R.id.img_profpic);
+				viewHolder.img_biz_pic = (ImageView)rowView.findViewById(R.id.img_biz_pic);
+				viewHolder.txt_biz_name = (TextView)rowView.findViewById(R.id.txt_biz_name);
+				viewHolder.txt_biz_loc = (TextView)rowView.findViewById(R.id.txt_biz_loc);
+				viewHolder.rb_ratings = (RatingBar)rowView.findViewById(R.id.rb_ratings);
 
 				rowView.setTag(viewHolder);
 			}
-
 			ViewHolder viewHolder = (ViewHolder) rowView.getTag();
-
-			// TODO set data
-			// viewHolder.img_profpic;
-			// viewHolder.img_mediaPic;
-			// viewHolder.img_MarkText;
-			// viewHolder.txt_sender_message;
-			// viewHolder.txt_sender_name;
-			// viewHolder.txt_sender_phone;
-			// viewHolder.txt_sender_time;
-			// viewHolder.txt_comment_count;
-			// viewHolder.txt_message_type;
-
-			// dummy stuff TODO remove this
-
-			// TODO format dates, set Fonts
-
+			//viewHolder.img_biz_pic
+			//TODO add imageloader
+			viewHolder.txt_biz_name.setText(data.get(pos).getBusiness_name());
+			viewHolder.txt_biz_loc.setText(data.get(pos).getAddress());
+			viewHolder.rb_ratings.setRating(data.get(pos).getStars());
 			return rowView;
-		}
-
-		int getRandomNo() {
-			return new Random(200719293).nextInt();
 		}
 	}
 
 	// list adapter
-	public class SearchListAdapter extends ViewInflaterBaseAdapter<String> {
+	public class SearchListAdapter extends ViewInflaterBaseAdapter<User> {
 
-		public SearchListAdapter(ViewInflater inflater, ArrayList<String> data) {
+		public SearchListAdapter(ViewInflater inflater, ArrayList<User> data) {
 			super(inflater, data);
 			super.setInflater(inflater);
 		}
@@ -177,4 +309,71 @@ public class SearchActivity extends DashboardActivity implements OnItemClickList
 		companyIntent.putExtras(args);
 		startActivity(companyIntent);
 	}
+	
+	class NetOps extends AsyncTask<String, Void, String> {
+		InputStream inputStream;
+		HttpClient httpclient;
+		HttpPost httppost;
+		List<NameValuePair> nameValuePairs;
+		StringBuffer sb;
+		
+		Runnable showToast = new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				toast("Unable to connect to the internet");
+			}
+		};
+
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			loading.setVisibility(View.VISIBLE);
+			data.clear();
+//			if (pDlg != null)
+//				pDlg.show();
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			String searchStr = params[0];
+
+			String results = null;
+			try {
+				URI uri = buildURI(searchStr);
+				results = new NetHandler().callAPI(uri);
+				Log.e(LOG_TAG, "result:"+results);
+			} catch (ClientProtocolException e) {
+				loadingError = true;
+				Log.e(LOG_TAG, "Api error: "+e.getMessage());
+				runOnUiThread(showToast);
+			} catch (IOException e) {
+				loadingError = true;
+				Log.e(LOG_TAG, "Api error: "+e.getMessage());
+				runOnUiThread(showToast);
+			} catch (URISyntaxException e) {
+				loadingError = true;
+				Log.e(LOG_TAG, "Api error: "+e.getMessage());
+				runOnUiThread(showToast);
+			}
+			return results;
+		}
+		
+		URI buildURI(String searchStr) throws URISyntaxException{
+			ArrayList<NameValuePair> args = new ArrayList<NameValuePair>();
+			args.add(new BasicNameValuePair(Constants.API_ENDPOINT_ARG, Constants.API_ENDPOINT_SEARCH));
+			args.add(new BasicNameValuePair(Constants.API_SEARCH_ARGS_QUERY, searchStr));
+			return URIUtils.createURI("http", Constants.API_BASE_URL, -1, Constants.API_ENDPOINT_URL, URLEncodedUtils.format(args, "UTF-8"), null);
+		}
+
+		@Override
+		protected void onPostExecute(String results) {
+			super.onPostExecute(results);
+			loading.setVisibility(View.GONE);
+//			if (pDlg != null)
+//				pDlg.dismiss();
+		}
+
+	}	
 }
