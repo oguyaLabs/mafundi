4b0858260563cc054e073111e687133c40ed0ff0
oguya <oguyajames@gmail.com>
2014-06-19 00:04:53 +0300

    DashBoard:: changing return type to User model

--- src/com/wglxy/example/dashL/DashboardActivity.java
+++ src/com/wglxy/example/dashL/DashboardActivity.java
@@ -21,13 +21,8 @@ package com.wglxy.example.dashL;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceManager;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -35,13 +30,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.wglxy.example.dashL.constants.Constants;
+import com.wglxy.example.dashL.model.User;
 import com.wglxy.example.dashL.utils.PrefUtils;
 
 /**
@@ -184,7 +178,7 @@ public boolean isLoggedIn(){
 	return new PrefUtils(this).isLoggedIn();
 }
 
-public String getLoggedInUser(){
+public User getLoggedInUser(){
 	return new PrefUtils(this).getLoggedInUser();
 }
 
