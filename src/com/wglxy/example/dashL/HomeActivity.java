869a335e9148b5f54eb77aa869950a12e0685cf0
oguya <oguyajames@gmail.com>
2014-06-19 01:04:32 +0300

    HomeActivity::reformatting files

--- src/com/wglxy/example/dashL/HomeActivity.java
+++ src/com/wglxy/example/dashL/HomeActivity.java
@@ -16,8 +16,6 @@
 
 package com.wglxy.example.dashL;
 
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -31,168 +29,162 @@ import android.widget.EditText;
 import com.wglxy.example.dashL.constants.Constants;
 
 /**
- * This is a simple activity that demonstrates the dashboard user interface pattern.
- *
- */
-
-public class HomeActivity extends DashboardActivity 
-{
-
-/**
- * onCreate - called when the activity is first created.
- * Called when the activity is first created. 
- * This is where you should do all of your normal static set up: create views, bind data to lists, etc. 
- * This method also provides you with a Bundle containing the activity's previously frozen state, if there was one.
+ * This is a simple activity that demonstrates the dashboard user interface
+ * pattern.
  * 
- * Always followed by onStart().
- *
  */
+
+public class HomeActivity extends DashboardActivity {
+
+	/**
+	 * onCreate - called when the activity is first created. Called when the
+	 * activity is first created. This is where you should do all of your normal
+	 * static set up: create views, bind data to lists, etc. This method also
+	 * provides you with a Bundle containing the activity's previously frozen
+	 * state, if there was one.
+	 * 
+	 * Always followed by onStart().
+	 * 
+	 */
 
 	EditText edt_search;
 	Button btn_search;
-	
-protected void onCreate(Bundle savedInstanceState) 
-{
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
-    showLogginLogout(findViewById(R.id.btn_login_logout));
-    
-    edt_search = (EditText)findViewById(R.id.edit_search);
-    btn_search = (Button)findViewById(R.id.btn_search);
-    
-    btn_search.setOnClickListener(clickListener);
-    edt_search.addTextChangedListener(watcher);
-}
 
-TextWatcher watcher = new TextWatcher() {
-	@Override
-	public void onTextChanged(CharSequence s, int start, int before, int count) {}
-	
-	@Override
-	public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-	
-	@Override
-	public void afterTextChanged(Editable editable) {
-		if (edt_search.getText().length() > 3){
-			btn_search.setEnabled(true);
-		}else{
-			btn_search.setEnabled(false);
-		}
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+		showLogginLogout(findViewById(R.id.btn_login_logout));
+
+		edt_search = (EditText) findViewById(R.id.edit_search);
+		btn_search = (Button) findViewById(R.id.btn_search);
+
+		btn_search.setOnClickListener(clickListener);
+		edt_search.addTextChangedListener(watcher);
 	}
-};
 
-View.OnClickListener clickListener = new View.OnClickListener() {
-	
-	@Override
-	public void onClick(View view) {
-		switch(view.getId()){
-		case R.id.btn_search:
-			  String search_str = edt_search.getText().toString();
-			  if(TextUtils.isEmpty(search_str)){
-				  toast("Please specify what you want to search for.");
-				  return;
-			  }
-			  Bundle args = new Bundle();
-			  args.putString(Constants.KEY_SEARCH_ARGS, search_str);
-			  Log.e("HomeActivity", "search: "+search_str);
-			  Intent searchIntent = new Intent(HomeActivity.this, SearchActivity.class);
-			  searchIntent.putExtras(args);
-			  startActivity(searchIntent);
+	TextWatcher watcher = new TextWatcher() {
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
 		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+		}
+
+		@Override
+		public void afterTextChanged(Editable editable) {
+			if (edt_search.getText().length() > 3) {
+				btn_search.setEnabled(true);
+			} else {
+				btn_search.setEnabled(false);
+			}
+		}
+	};
+
+	View.OnClickListener clickListener = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View view) {
+			switch (view.getId()) {
+			case R.id.btn_search:
+				String search_str = edt_search.getText().toString();
+				if (TextUtils.isEmpty(search_str)) {
+					toast("Please specify what you want to search for.");
+					return;
+				}
+				Bundle args = new Bundle();
+				args.putString(Constants.KEY_SEARCH_ARGS, search_str);
+				Log.e("HomeActivity", "search: " + search_str);
+				Intent searchIntent = new Intent(HomeActivity.this,
+						SearchActivity.class);
+				searchIntent.putExtras(args);
+				startActivity(searchIntent);
+			}
+		}
+	};
+
+	/**
+	 * onDestroy The final call you receive before your activity is destroyed.
+	 * This can happen either because the activity is finishing (someone called
+	 * finish() on it, or because the system is temporarily destroying this
+	 * instance of the activity to save space. You can distinguish between these
+	 * two scenarios with the isFinishing() method.
+	 * 
+	 */
+
+	protected void onDestroy() {
+		super.onDestroy();
 	}
-};
-    
-/**
- * onDestroy
- * The final call you receive before your activity is destroyed. 
- * This can happen either because the activity is finishing (someone called finish() on it, 
- * or because the system is temporarily destroying this instance of the activity to save space. 
- * You can distinguish between these two scenarios with the isFinishing() method.
- *
+
+	/**
+	 * onPause Called when the system is about to start resuming a previous
+	 * activity. This is typically used to commit unsaved changes to persistent
+	 * data, stop animations and other things that may be consuming CPU, etc.
+	 * Implementations of this method must be very quick because the next
+	 * activity will not be resumed until this method returns. Followed by
+	 * either onResume() if the activity returns back to the front, or onStop()
+	 * if it becomes invisible to the user.
+	 * 
+	 */
+
+	protected void onPause() {
+		super.onPause();
+	}
+
+	/**
+	 * onRestart Called after your activity has been stopped, prior to it being
+	 * started again. Always followed by onStart().
+	 * 
+	 */
+
+	protected void onRestart() {
+		super.onRestart();
+	}
+
+	/**
+	 * onResume Called when the activity will start interacting with the user.
+	 * At this point your activity is at the top of the activity stack, with
+	 * user input going to it. Always followed by onPause().
+	 * 
+	 */
+
+	protected void onResume() {
+		super.onResume();
+	}
+
+	/**
+	 * onStart Called when the activity is becoming visible to the user.
+	 * Followed by onResume() if the activity comes to the foreground, or
+	 * onStop() if it becomes hidden.
+	 * 
+	 */
+
+	protected void onStart() {
+		super.onStart();
+	}
+
+	/**
+	 * onStop Called when the activity is no longer visible to the user because
+	 * another activity has been resumed and is covering this one. This may
+	 * happen either because a new activity is being started, an existing one is
+	 * being brought in front of this one, or this one is being destroyed.
+	 * 
+	 * Followed by either onRestart() if this activity is coming back to
+	 * interact with the user, or onDestroy() if this activity is going away.
+	 */
+
+	protected void onStop() {
+		super.onStop();
+	}
+
+	/**
  */
+	// Click Methods
 
-protected void onDestroy ()
-{
-   super.onDestroy ();
-}
-
-/**
- * onPause
- * Called when the system is about to start resuming a previous activity. 
- * This is typically used to commit unsaved changes to persistent data, stop animations 
- * and other things that may be consuming CPU, etc. 
- * Implementations of this method must be very quick because the next activity will not be resumed 
- * until this method returns.
- * Followed by either onResume() if the activity returns back to the front, 
- * or onStop() if it becomes invisible to the user.
- *
+	/**
  */
-
-protected void onPause ()
-{
-   super.onPause ();
-}
-
-/**
- * onRestart
- * Called after your activity has been stopped, prior to it being started again.
- * Always followed by onStart().
- *
- */
-
-protected void onRestart ()
-{
-   super.onRestart ();
-}
-
-/**
- * onResume
- * Called when the activity will start interacting with the user. 
- * At this point your activity is at the top of the activity stack, with user input going to it.
- * Always followed by onPause().
- *
- */
-
-protected void onResume ()
-{
-   super.onResume ();
-}
-
-/**
- * onStart
- * Called when the activity is becoming visible to the user.
- * Followed by onResume() if the activity comes to the foreground, or onStop() if it becomes hidden.
- *
- */
-
-protected void onStart ()
-{
-   super.onStart ();
-}
-
-/**
- * onStop
- * Called when the activity is no longer visible to the user
- * because another activity has been resumed and is covering this one. 
- * This may happen either because a new activity is being started, an existing one 
- * is being brought in front of this one, or this one is being destroyed.
- *
- * Followed by either onRestart() if this activity is coming back to interact with the user, 
- * or onDestroy() if this activity is going away.
- */
-
-protected void onStop ()
-{
-   super.onStop ();
-}
-
-/**
- */
-// Click Methods
-
-
-/**
- */
-// More Methods
+	// More Methods
 
 } // end class
